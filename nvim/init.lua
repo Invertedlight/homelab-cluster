dd65ad828f4f017ee62206ae3bd8da6e8bc7d65a
Invertedlight <james.richardson@invertedlight.com>
2026-08-20 18:34:34 +0000

    FEAT: Enhance nvim to allow copy past to Mac

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,23 @@
 if vim.fn.has("nvim-0.10") == 1 then
-  vim.g.clipboard = "osc52"
+	vim.g.clipboard = "osc52"
 end
 
 local function copy()
-  return { vim.fn.getreg('"') }
+	return { vim.fn.getreg('"') }
 end
 
 local function paste()
-  return vim.fn.split(vim.fn.getreg('"'), "\n")
+	return vim.fn.split(vim.fn.getreg('"'), "\n")
 end
 
 vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = copy,
-    ["*"] = copy,
-  },
-  paste = {
-    ["+"] = paste,
-    ["*"] = paste,
-  },
+	name = "OSC 52",
+	copy = {
+		["+"] = copy,
+		["*"] = copy,
+	},
+	paste = {
+		["+"] = paste,
+		["*"] = paste,
+	},
 }
-
