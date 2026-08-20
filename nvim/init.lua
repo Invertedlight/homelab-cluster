5a829c2576053e5a7f26d6bfcf3b86af440b1e73
Invertedlight <james.richardson@invertedlight.com>
2026-08-20 18:33:29 +0000

    Add: configmap and audiobookshelf-deployment

---
+++ nvim/init.lua
@@ -0,0 +1,24 @@
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.g.clipboard = "osc52"
+end
+
+local function copy()
+  return { vim.fn.getreg('"') }
+end
+
+local function paste()
+  return vim.fn.split(vim.fn.getreg('"'), "\n")
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = copy,
+    ["*"] = copy,
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
